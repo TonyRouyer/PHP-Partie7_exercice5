ad100bee9262d8856a6f512594c25b8f59cc43aa
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-19 15:10:57 +0200

    correction

--- index.php
+++ index.php
@@ -1,49 +1,67 @@
+<?php
+$regexName = '^[\p{L}]{1}[\' \-\p{L}]+$^';
+$formErrors = array();
+if(count($_POST)>0){
+    if(!empty($_POST['civility'])) {
+        if($_POST['civility'] == 'Monsieur' || $_POST['civility'] == 'Madame') {
+            $civility = $_POST['civility'];
+        }else{
+            $formErrors['civility'] = 'Votre civilité est mal choisie';
+        }
+    }else{
+        $formErrors['civility'] = 'Votre civilité n\'est pas renseignée';
+    }
+
+    if(!empty($_POST['lastName'])) {
+        if(preg_match($regexName, $_POST['lastName'])) {
+            $lastName = $_POST['lastName'];
+        }else{
+            $formErrors['lastName'] = 'Votre nom de famille n\'est pas valide';
+        }
+    }else{
+        $formErrors['lastName'] = 'Votre nom de famille n\'est pas renseigné';
+    }
+
+    if(!empty($_POST['firstName'])) {
+        if(preg_match($regexName, $_POST['firstName'])) {
+            $firstName = $_POST['firstName'];
+        }else{
+            $formErrors['firstName'] = 'Votre prénom n\'est pas valide';
+        }
+    }else{
+        $formErrors['firstName'] = 'Votre prénom n\'est pas renseigné';
+    }
+}
+?> 
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>partie 7 exercice 5</title>
-        <style>
-            form {
-                display: flex;
-                flex-direction: column;
-                width: 50%;
-                margin: 0 auto;
-            }
-            label {
-                margin-top: 10px;
-            }
-            #firstname, #lastname {
-                border: 0px;
-                border-bottom: 1px Solid black;
-            }
-            #sendBtn {
-                width: 10%;
-                margin-top: 10px;
-            }
-        </style>
-    </head>
-    <body>
-        <form action="index.php" method="POST">
-            <label for="civilite">
-                Civilité :
-                <select name="civilite" id="civilite">
-                    <option disabled selected>Sélectionner</option>
-                    <option value="Mr">Monsieur</option>
-                    <option value="Mme">Madame</option>
-                </select>
-            </label>
-            <label for="firstname">Prénom : <input type="text" id="firstname" name="firstname" /></label>
-            <label for="lastname">Nom : <input type="text" id="lastname" name="lastname" /></label>
-            <input type="submit" id="sendBtn" />
-        </form>
-        <?php 
-            if ( isset($_POST['civilite']) && preg_match("/Mr|Mme/", $_POST['firstname']) &&
-                 isset($_POST['lastname']) && preg_match("/^[a-zA-Z]+[-\s]?[a-z]+$/", $_POST['firstname']) &&
-                 isset($_POST['firstname']) && preg_match("/^[a-zA-Z]+[-\s]?[a-z]+$/", $_POST['firstname'])
-                 ) {?>
-            <p><?= 'bonjour ' . htmlspecialchars($_POST['civilite']) . ' ' . htmlspecialchars($_POST['lastname']) . ' ' . htmlspecialchars($_POST['firstname']) .  ', vous aller bien ?' ?></p>
-            <?php }?>
-    </body>
+<head>
+    <meta charset="UTF-8" />
+    <title>php partie7 exo5</title>
+</head>
+<body>
+<!--Création du formulaire-->
+    <form method="POST" action="index.php">
+        <label for="civility">Civilité</label>
+        <select id="civility" name="civility">
+            <option disabled selected>Sélectionner</option>
+            <option <?= isset($_POST['civility']) && $_POST['civility'] == 'Monsieur' ? 'selected' : '' ?> value="Monsieur">Mr</option>
+            <option <?= isset($_POST['civility']) && $_POST['civility'] == 'Madame' ? 'selected' : '' ?> value="Madame">Mme</option>
+        </select>
+        <?php if(isset($formErrors['civility'])) { ?>
+        <p><?= $formErrors['civility'] ?></p>
+        <?php } ?>
+        <!-- On vérifie qu'on envoie le nom de famille. Si c'est le cas on l'affiche dans le champ-->
+        <p><label for="lastName">Votre nom : </label><input type="text" id="lastName" name="lastName" <?= isset($_POST['lastName']) ? 'value="' . $_POST['lastName'] . '"' : '' ?> /></p>
+        <?php if(isset($formErrors['lastName'])) { ?>
+        <p><?= $formErrors['lastName'] ?></p>
+        <?php } ?>
+        <p><label for="firstName">Votre prénom : </label><input type="text" id="firstName" name="firstName" <?=isset($_POST['firstName']) ? 'value="' . $_POST['firstName'] . '"' : '' ?> /></p>
+        <?php if(isset($formErrors['firstName'])) { ?>
+        <p><?= $formErrors['firstName'] ?></p>
+        <?php } ?>
+        <input type="submit" value="Envoyer" />
+
+    </form>
+</body>
 </html>
